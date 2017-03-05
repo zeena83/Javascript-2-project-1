6f25a4d703055bdba748e473fb86da861a0ec0dd
zeena83 <zeena_aywaz@yahoo.com>
2017-03-05 20:26:51 +0100

    Add files via upload

---
+++ login/log.js
@@ -0,0 +1,22 @@
+function clicked(){
+	var user = document.getElementById ('username');
+	var pass = document.getElementById ('password');
+	
+
+var coruser = "test";
+var corpass = "123";
+
+if(user.value == coruser) {
+	if(pass.value == corpass){
+		window.alert("You are logged in as" + user.value);
+		window.open("https://www.facebook.com");
+	
+}else{
+		window.alert("Incorrect username or password!");
+	}
+}else{
+	window.alert("Incorrect username or password!");
+}
+}
+
+
